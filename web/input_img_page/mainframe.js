8ddde01face02160026b5f5f97e39676e2c9d499
HDLY <42459504+HandeulLy@users.noreply.github.com>
2020-05-15 17:44:39 +0900

    image_input_page(by_HDLY)

---
+++ web/input_img_page/mainframe.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    push = false;
+    
+    $('ul li a').click(function () {
+        $(".tab").removeClass("active");
+        $(this).addClass("active");
+        $(".container").removeClass("actives");
+        var contentId = $(this).attr("href");
+
+        $('.container[id="' + contentId + '"]').addClass("actives");
+        if (!push)
+            history.pushState({}, '', contentId);
+        push = false;
+        return false;
+    });
+
+    $(window).on("popstate", function () {
+        push = true;
+        var h = (window.location.href.indexOf("/") > -1) ? window.location.href.split("/").pop() : false;
+        if (h == 'home.php') {
+            $('ul li a[href="Home"]').click();
+        } else {
+            $('ul li a[href="' + h + '"]').click();
+        }
+    });    
+});
