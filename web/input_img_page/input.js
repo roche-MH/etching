d90998dc26c495bddf194cb0c1395b4bf829d154
roche-MH <53164586+roche-MH@users.noreply.github.com>
2020-05-22 13:50:36 +0900

    CSS 수정 : MH

--- web/input_img_page/input.js
+++ web/input_img_page/input.js
@@ -1,4 +1,4 @@
-function readURL(input) {
+function srcURL(input) {
     if (input.files && input.files[0]) {
 
         var reader = new FileReader();
@@ -9,7 +9,28 @@ function readURL(input) {
             $('.file-upload-image').attr('src', e.target.result);
             $('.file-upload-content').show();
 
-            $('.image-title').html(input.files[0].name);
+            //$('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+
+    } else {
+        removeUpload();
+    }
+}
+
+function refURL(input) {
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            //$('.image-title').html(input.files[0].name);
         };
 
         reader.readAsDataURL(input.files[0]);
