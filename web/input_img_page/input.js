1e8b116fb87dc1ea76200a1d46068497463a9726
HDLY <42459504+HandeulLy@users.noreply.github.com>
2020-05-29 17:31:35 +0900

    Image_uploaded_separately:HDLY

--- web/input_img_page/input.js
+++ web/input_img_page/input.js
@@ -1,53 +1,51 @@
 function srcURL(input) {
     if (input.files && input.files[0]) {
-
         var reader = new FileReader();
-
         reader.onload = function (e) {
-            $('.image-upload-wrap').hide();
-
-            $('.file-upload-image').attr('src', e.target.result);
-            $('.file-upload-content').show();
-
-            //$('.image-title').html(input.files[0].name);
+            $('.srcimg-upload-wrap').hide();
+            $('.file-upload-image_src').attr('src', e.target.result);
+            $('.file-upload-content_src').show();
         };
-
         reader.readAsDataURL(input.files[0]);
-
     } else {
-        removeUpload();
+        removeUpload_src();
     }
 }
 
 function refURL(input) {
     if (input.files && input.files[0]) {
-
         var reader = new FileReader();
-
         reader.onload = function (e) {
-            $('.image-upload-wrap').hide();
-
-            $('.file-upload-image').attr('src', e.target.result);
-            $('.file-upload-content').show();
-
-            //$('.image-title').html(input.files[0].name);
+            $('.refimg-upload-wrap').hide();
+            $('.file-upload-image_ref').attr('src', e.target.result);
+            $('.file-upload-content_ref').show();
         };
-
         reader.readAsDataURL(input.files[0]);
-
     } else {
-        removeUpload();
+        removeUpload_ref();
     }
 }
 
-function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
+function removeUpload_src() {
+    $('.file_upload_input_src').replaceWith($('.file_upload_input_src').clone());
+    $('.file-upload-content_src').hide();
+    $('.srcimg-upload-wrap').show();
 }
-$('.image-upload-wrap').bind('dragover', function () {
-    $('.image-upload-wrap').addClass('image-dropping');
+$('.srcimg-upload-wrap').bind('dragover', function () {
+    $('.srcimg-upload-wrap').addClass('srcimg-dropping');
 });
-$('.image-upload-wrap').bind('dragleave', function () {
-    $('.image-upload-wrap').removeClass('image-dropping');
+$('.srcimg-upload-wrap').bind('dragleave', function () {
+    $('.srcimg-upload-wrap').removeClass('srcimg-dropping');
+});
+
+function removeUpload_ref() {
+    $('.file_upload_input_ref').replaceWith($('.file_upload_input_ref').clone());
+    $('.file-upload-content_ref').hide();
+    $('.refimg-upload-wrap').show();
+}
+$('.refimg-upload-wrap').bind('dragover', function () {
+    $('.refimg-upload-wrap').addClass('refimg-dropping');
+});
+$('.refimg-upload-wrap').bind('dragleave', function () {
+    $('.refimg-upload-wrap').removeClass('refimg-dropping');
 });
